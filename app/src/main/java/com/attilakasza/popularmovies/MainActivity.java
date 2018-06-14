73af99c04f8c16f89a73e7726a4d45a19a301611
Attila <9ttila.kasza@gmail.com>
2018-06-14 22:12:34 +0200

    feat: Create PosterFragment

--- app/src/main/java/com/attilakasza/popularmovies/MainActivity.java
+++ app/src/main/java/com/attilakasza/popularmovies/MainActivity.java
@@ -5,9 +5,33 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PosterFragment mPosterFragment;
+    private static final String FRAGMENT_NAME = "POSTERFRAGMENT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            mPosterFragment = new PosterFragment();
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .add(R.id.poster_container, mPosterFragment)
+                    .commit();
+        }
+        if (savedInstanceState != null) {
+            mPosterFragment = (PosterFragment) getSupportFragmentManager().getFragment(savedInstanceState, FRAGMENT_NAME);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mPosterFragment != null) {
+            if (mPosterFragment.isAdded()) {
+                getSupportFragmentManager().putFragment(outState, FRAGMENT_NAME, mPosterFragment);
+            }
+        }
     }
 }
