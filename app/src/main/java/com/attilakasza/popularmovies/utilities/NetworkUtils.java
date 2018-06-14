e8e4824d7e03abe572ac9b82ce96f050e5028ead
Attila <9ttila.kasza@gmail.com>
2018-06-14 21:38:49 +0200

    feat: Fetch HTTP request

--- app/src/main/java/com/attilakasza/popularmovies/utilities/NetworkUtils.java
+++ app/src/main/java/com/attilakasza/popularmovies/utilities/NetworkUtils.java
@@ -4,8 +4,12 @@ import android.net.Uri;
 
 import com.attilakasza.popularmovies.BuildConfig;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class NetworkUtils {
 
@@ -29,5 +33,23 @@ public class NetworkUtils {
         }
 
         return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
