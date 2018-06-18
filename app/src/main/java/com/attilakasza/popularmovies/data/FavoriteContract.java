5be7bc08c3df59e43c179608a6d227ebbad2f8a8
Attila <9ttila.kasza@gmail.com>
2018-06-18 21:09:37 +0200

    feat: Add FavoriteContract class

---
+++ app/src/main/java/com/attilakasza/popularmovies/data/FavoriteContract.java
@@ -0,0 +1,25 @@
+package com.attilakasza.popularmovies.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class FavoriteContract {
+
+    public static final String AUTHORITY = "com.example.android.popularmovies";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final String PATH_FAVORITES = "favorites";
+
+    public static final class FavoriteEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVORITES).build();
+
+        public static final String TABLE_NAME = "favorites";
+        public static final String COLUMN_ID = "id";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_POSTER = "poster";
+        public static final String COLUMN_VOTE = "vote";
+        public static final String COLUMN_PLOT = "plot";
+        public static final String COLUMN_FAVORITE = "favorite";
+    }
+}
