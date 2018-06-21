132740c8a31ee88bdc3bbee3a15dd3ceb3cb3661
Attila <9ttila.kasza@gmail.com>
2018-06-21 07:18:55 +0200

    feat: Use ButterKnife library

--- app/src/main/java/com/attilakasza/popularmovies/activities/MovieActivity.java
+++ app/src/main/java/com/attilakasza/popularmovies/activities/MovieActivity.java
@@ -4,21 +4,47 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.attilakasza.popularmovies.R;
+import com.attilakasza.popularmovies.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private Movie mMovie;
+    private static final String MOVIE = "MOVIE";
+    private static final String POSTER_URL = "http://image.tmdb.org/t/p/w185";
+
+    @BindView(R.id.tv_date) TextView textDate;
+    @BindView(R.id.iv_poster) ImageView imagePoster;
+    @BindView(R.id.tv_vote) TextView textVote ;
+    @BindView(R.id.tv_plot) TextView textPlot ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+        ButterKnife.bind(this);
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        mMovie = getIntent().getParcelableExtra(MOVIE);
+
+        textVote.setText(mMovie.getmVote());
+        textPlot.setText(mMovie.getmPlotSynopsis());
+        textDate.setText("(" + mMovie.getmDate().substring(0, 4) + ")");
+        Picasso.with(this)
+                .load(POSTER_URL.concat(mMovie.getmPoster()))
+                .into(imagePoster);
     }
 
     @Override
--- app/src/main/java/com/attilakasza/popularmovies/fragments/MovieFragment.java
+++ app/src/main/java/com/attilakasza/popularmovies/fragments/MovieFragment.java
@@ -7,27 +7,48 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.attilakasza.popularmovies.R;
 import com.attilakasza.popularmovies.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MovieFragment extends Fragment {
 
     private Movie mMovie;
     private static final String MOVIE = "MOVIE";
+    private static final String POSTER_URL = "http://image.tmdb.org/t/p/w185";
+
+    @BindView(R.id.tv_title) TextView textTitle ;
+    @BindView(R.id.tv_date) TextView textDate;
+    @BindView(R.id.iv_poster) ImageView imagePoster;
+    @BindView(R.id.tv_vote) TextView textVote ;
+    @BindView(R.id.tv_plot) TextView textPlot ;
 
     public MovieFragment() {}
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_movie, container, false);
+        ButterKnife.bind(this, rootView);
 
         Bundle bundle = this.getArguments();
         if (bundle != null) {
             mMovie = bundle.getParcelable(MOVIE);
         }
+
+        textTitle.setText(mMovie.getmTitle());
+        textVote.setText(mMovie.getmVote());
+        textPlot.setText(mMovie.getmPlotSynopsis());
+        textDate.setText("(" + mMovie.getmDate().substring(0, 4) + ")");
+        Picasso.with(getContext())
+                .load(POSTER_URL.concat(mMovie.getmPoster()))
+                .into(imagePoster);
 
         return rootView;
     }
