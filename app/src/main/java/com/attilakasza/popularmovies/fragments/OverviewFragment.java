3dc2a5b95562d42aa43d8e67e7a7671d944283b7
Attila <9ttila.kasza@gmail.com>
2018-07-05 06:40:37 +0200

    feat: Polish UI of OverviewFragment

--- app/src/main/java/com/attilakasza/popularmovies/fragments/OverviewFragment.java
+++ app/src/main/java/com/attilakasza/popularmovies/fragments/OverviewFragment.java
@@ -5,16 +5,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.attilakasza.popularmovies.R;
 import com.attilakasza.popularmovies.models.Movie;
+import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class OverviewFragment extends Fragment {
 
     private Movie mMovie;
     private static final String MOVIE = "MOVIE";
+    private static final String POSTER_URL = "http://image.tmdb.org/t/p/w342";
+
+    @BindView(R.id.iv_poster) ImageView mImageViewPoster;
+    @BindView(R.id.tv_title) TextView mTextViewTitle;
+    @BindView(R.id.tv_date) TextView mTextViewDate;
+    @BindView(R.id.tv_vote) TextView mTextViewVote;
+    @BindView(R.id.tv_plot) TextView mTextViewPlot;
 
 
     public OverviewFragment() {
@@ -31,6 +42,13 @@ public class OverviewFragment extends Fragment {
         if (bundle != null) {
             mMovie = bundle.getParcelable(MOVIE);
         }
+
+        mTextViewDate.setText("(" + mMovie.getmDate().substring(0, 4) + ")");
+        Picasso.with(getContext())
+                .load(POSTER_URL.concat(mMovie.getmPoster()))
+                .into(mImageViewPoster);
+        mTextViewVote.setText(mMovie.getmVote());
+        mTextViewPlot.setText(mMovie.getmPlotSynopsis());
 
         return rootView;
     }
