c1fd0adf2d9f40fa3e21f07fc0ecda5b191d2214
Attila <9ttila.kasza@gmail.com>
2018-08-02 16:14:35 +0200

    style: Modify color of warning message

--- app/src/main/java/com/attilakasza/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/attilakasza/popularmovies/activities/MainActivity.java
@@ -226,7 +226,7 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnIt
 
             View sbView = snackbar.getView();
             TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
-            textView.setTextColor(Color.RED);
+            textView.setTextColor(getResources().getColor(R.color.colorSnackbar));
             snackbar.show();
         }
     }
